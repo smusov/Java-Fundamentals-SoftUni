96612656ce65794ee94fa7ea1895d7f735fc24ec
Slavi <slaveizlatarev@gmail.com>
2019-07-16 00:53:09 +0300

    Add Student Information .

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/src/Passed.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Passed {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        double grade = Double.parseDouble(read.readLine());
+
+        if (grade>=3.00){
+            System.out.println("Passed");
+        }
+
+    }
+}
