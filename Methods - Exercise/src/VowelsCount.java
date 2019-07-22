6fb6cc9c0b0e53fdb63a6722760e9602906c51fe
Slavi <slaveizlatarev@gmail.com>
2019-07-22 17:53:43 +0300

    Add task Vowels Count .

---
+++ Methods - Exercise/src/VowelsCount.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class VowelsCount {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = read.readLine().toLowerCase().split("");
+        System.out.println(vowelsCount(input));
+    }
+
+    private static int vowelsCount(String[] word) {
+        String sym = "aeoui";
+        int sum = 0;
+
+        for (String s : word) {
+            if (sym.contains(s)) {
+                sum++;
+            }
+        }
+        return sum;
+    }
+}
